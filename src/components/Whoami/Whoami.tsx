c5d47882a24a2ccbb976f09a5ed0332887b595c5
Sam Tregea <sdtregea@gmail.com>
2025-07-26 16:10:10 -0400

    Whitespacing

--- src/components/Whoami/Whoami.tsx
+++ src/components/Whoami/Whoami.tsx
@@ -13,25 +13,25 @@ import FadeIn from "@/components/utils/FadeIn/FadeIn";
  * @returns {JSX.Element} The rendered Whoami section.
  */
 export default function Whoami() {
-  return (
-    <section className={styles.whoami} id="whoami">
-        <div className={styles.heading}>
-            {"hi, im "}
-            <span className={styles.typeWriter}>
-                <Typewriter
-                    words={["an engineer", "a creator", "sam"]}
-                    loop={1}
-                    cursor={true}
-                    cursorStyle="|"
-                    typeSpeed={100}
-                />
-            </span>
-        </div>
-        <FadeIn delay={7}>
-            <div className={styles.description}>
-                {"I also like to build things with code."}
+    return (    
+        <section className={styles.whoami} id="whoami">
+            <div className={styles.heading}>
+                {"hi, im "}
+                <span className={styles.typeWriter}>
+                    <Typewriter
+                        words={["an engineer", "a creator", "sam"]}
+                        loop={1}
+                        cursor={true}
+                        cursorStyle="|"
+                        typeSpeed={100}
+                    />
+                </span>
             </div>
-        </FadeIn>
-    </section>
+            <FadeIn delay={7}>
+                <div className={styles.description}>
+                    {"I also like to build things with code."}
+                </div>
+            </FadeIn>
+        </section>
   );
 };
