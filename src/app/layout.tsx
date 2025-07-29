c6df25d0fdb30d72106bdeb0df658f08fce24cab
Sam Tregea <sdtregea@gmail.com>
2025-07-29 11:59:45 -0400

    Add Vercel's Analytics component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from '@vercel/analytics/next';
 
 export const metadata: Metadata = {
   title: "Samuel Tregea",
@@ -16,6 +17,7 @@ export default function RootLayout({ children,}: Readonly<{ children: React.Reac
       <body>
         {children}
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
