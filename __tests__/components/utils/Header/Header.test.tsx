a761dc77cdd74a6b834417459a676bd2e599f87c
Sam Tregea <sdtregea@gmail.com>
2025-08-07 14:29:12 -0400

    Create unit test for the Header component

---
+++ __tests__/components/utils/Header/Header.test.tsx
@@ -0,0 +1,30 @@
+import { describe, test, expect, beforeEach, afterEach } from "vitest";
+import { render, screen, cleanup } from "@testing-library/react";
+import Header from "@/components/utils/Header/Header";
+
+/**
+ * Header Test Suite
+ * 
+ * This suite tests the Header component, ensuring it renders correctly.
+ */
+describe("Header", () => {
+    beforeEach(() => {
+        render(<Header>{"Test Header"}</Header>);
+    });
+
+    afterEach(() => {
+        cleanup();
+    });
+
+    /**
+     * Test to verify the rendering of the Header component.
+     */
+    test("the rendering of the Header component", () => {
+        const header = screen.getByRole("heading", { level: 2 });
+        
+        expect(header).toBeDefined();
+        expect(header.localName).toEqual("h2");
+        expect(header.innerHTML).toEqual("Test Header");
+        expect(header.className).toContain("header");
+    });
+});
