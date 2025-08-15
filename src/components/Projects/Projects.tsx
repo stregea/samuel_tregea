dec5a03c4e889c92a96253d252be632e01d26919
Sam Tregea <sdtregea@gmail.com>
2025-08-15 09:10:28 -0400

    Add Pagination to projects

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,10 +1,12 @@
+import { useState } from "react";
 import styles from "./Projects.module.css";
 import Section from "@/components/utils/Section/Section";
 import Header from "@/components/utils/Header/Header";
 import Grid from "@mui/material/Grid";
 import FadeIn from "@/components/utils/FadeIn/FadeIn";
-import ProjectCard from "./ProjectCard/ProjectCard";
+import ProjectCard from "@/components/Projects/ProjectCard/ProjectCard";
 import projectData from "@/data/projects.json";
+import Pagination from "@mui/material/Pagination";
 
 /**
  * Renders the "Projects" section of the website, displaying my personal and college projects
@@ -17,6 +19,23 @@ import projectData from "@/data/projects.json";
  * @returns {JSX.Element} The rendered Projects component.
  */
 export default function Projects () {
+	// Set the max number of projects per page.
+	const projectsPerPage = 6;
+
+	// Calculate the total number of pages.
+    const totalPages = Math.ceil(projectData.projects.length / projectsPerPage);
+
+	// Page state
+	const [page, setPage] = useState(1);
+
+	// Handler for page change.
+	const handleChange = (event: React.ChangeEvent<unknown>, page: number) => {
+		setPage(page);
+	};
+
+	// Slice the projects for the current page.
+	const projects = projectData.projects.slice((page - 1) * projectsPerPage, page * projectsPerPage);
+
 	return (
 		<Section id="projects">
 			<Header>{"// projects"}</Header>
@@ -27,8 +46,9 @@ export default function Projects () {
 					columnSpacing={2}
 					justifyContent={"center"}
 					alignItems={"center"}
+					data-testid={"project-grid"}
 				>
-					{projectData.projects.map((project, index) => (
+					{projects.map((project, index) => (
 						<FadeIn key={index} delay={index * 0.2}>
 							<ProjectCard
 								key={index}
@@ -38,6 +58,14 @@ export default function Projects () {
 					))}
 				</Grid>
 			</div>
+			<div className={styles.pagination}>
+				<Pagination
+					count={totalPages}
+					page={page}
+					onChange={handleChange}
+					data-testid={"project-pagination"}
+				/>
+			</div>
 		</Section>
-	); 
+	);
 };
