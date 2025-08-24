740b649a7f159a312a44d51a7f63742d80339132
Sam Tregea <sdtregea@gmail.com>
2025-08-24 10:18:59 -0400

    Create a unit test for the LinkedIn icon

---
+++ __tests__/components/utils/Icons/LinkedIn/LinkedIn.test.tsx
@@ -0,0 +1,29 @@
+import { describe, test, expect, beforeEach, afterEach } from "vitest";
+import { render, screen, cleanup } from "@testing-library/react";
+import LinkedIn from "@/components/utils/Icons/LinkedIn/LinkedIn";
+
+/**
+ * LinkedIn Test Suite
+ *
+ * This suite tests the LinkedIn component, ensuring it renders correctly.
+ */
+describe("LinkedIn Icon", () => {
+    beforeEach(() => {
+        render(<LinkedIn url={"https://www.linkedin.com/in/username"} />);
+    });
+
+    afterEach(() => {
+        cleanup();
+    });
+
+    /**
+     * Test to verify the rendering of the LinkedIn component.
+     */
+    test("the rendering of the LinkedIn icon.", () => {
+        const button = screen.getByTestId("linkedin-button");
+        const icon = screen.getByTestId("linkedin-icon");
+
+        expect(button).toBeDefined();
+        expect(icon).toBeDefined();
+    });
+});
